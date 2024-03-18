1feda43f3e9d0971da53c2f44622a88ee0ac2a39
Suraj Yadav <surajyadav200701@gmail.com>
2024-03-18 22:15:24 +0530

    enums starter pack

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spectrum/enums/enums.dart';
+
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const Enums(),
+      theme: ThemeData.from(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xff005c9a),
+          brightness: Brightness.dark,
+          surface: const Color(0xFF17202C),
+          background: const Color(0xFF0E1215),
+        ),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
